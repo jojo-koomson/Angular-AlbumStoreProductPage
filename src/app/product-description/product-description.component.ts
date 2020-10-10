1137a30825761ae4366b700fe530ba465aae8dbd
jojo-koomson <53906917+jojo-koomson@users.noreply.github.com>
2020-10-10 07:56:52 +0000

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -10,7 +10,6 @@ export class ProductDescriptionComponent implements OnInit {
 	albumInfo;
   constructor(private _productService: ProductService) { }
 
-  ngOnInit() {
   	this._productService.getAlbum(1).subscribe(response => 
   	this.albumInfo = response);
   }
