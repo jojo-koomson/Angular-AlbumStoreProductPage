18c517ff3eab8a471f983e763f4a1064753281fc
jojo-koomson <53906917+jojo-koomson@users.noreply.github.com>
2020-10-10 07:15:49 +0000

    s

---
+++ src/app/product.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {http, Response} from '@angular/http';
+
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http:Http) { }
+
+}
