d69374022b9f68ed83692caacc25dad257b7ccba
jojo-koomson <53906917+jojo-koomson@users.noreply.github.com>
2020-10-12 00:53:15 +0000

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,8 +12,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id:number): obervable<Album>{
-  	return this._http.get(this._albumUrl).map((response => <Album> response.json());
+  getAlbum(id:number): Observable<Album>{
+  	return this._http.get(this._albumUrl).map((response => <Album>response.json());
   	 
   }
 }
